63ce9724b439ae99ff387e9a31caa74b64946ae6
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-13 00:30:03 -0800

    gets name and description

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -5,17 +5,21 @@ export default function WeatherInfo({ currentWeather }) {
 
     const { 
         main : { temp },
-        weather : [details]
+        weather : [details],
+        name,
     } = currentWeather
 
-    const {icon} = details
+    const { icon, main, description } = details
 
     const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
 
     return (
         <View style={ styles.WeatherInfo }>
+            <Text>{name}</Text>
             <Image style={ styles.weatherIcon } source={{ uri: iconUrl }} />
             <Text>{temp}</Text>
+            <Text>{description}</Text>
+            <Text>{main}</Text>
         </View>
     )
 }

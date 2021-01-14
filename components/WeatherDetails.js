29e4f03e9ad02848a2390ef87b2c3f0b40eee5dd
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-13 20:17:46 -0800

    structures views for each item

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { colors } from '../utils/index'
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons'
 
 const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors
 
@@ -14,7 +15,13 @@ export default function WeatherDetails({ currentWeather }) {
         <View style={ styles.weatherDetails }>
             <View style={ styles.weatherDetailsRow }>
                 <View style={ {...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR} }>
-                    <Text>{ feels_like }</Text>
+                    <View style={ styles.weatherDetailsRow }>
+                        <FontAwesome5 name="temperature-low" size={ 25 } color={ PRIMARY_COLOR } />
+                        <View style={ styles.weatherDetailsItems } >
+                            <Text>Feels like:</Text>
+                            <Text style={ styles.textSecondary }>{ feels_like }</Text>
+                        </View>
+                    </View>
                 </View>
                 <View style={ styles.weatherDetailsBox }>
                     <Text>{ humidity }</Text>
@@ -42,6 +49,18 @@ const styles = StyleSheet.create ({
     weatherDetailsBox: {
         flex: 1,
         padding: 20
+    },
+
+    weatherDetailsItems: {
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end'
+    },
+
+    textSecondary: {
+        fontSize: 15,
+        color: SECONDARY_COLOR,
+        fontWeight: '700',
+        margin: 7
     }
 
 })

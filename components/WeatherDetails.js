85af59db1998c188c467d5661e256238d28ec1da
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-13 20:28:00 -0800

    creates function to determine windpseed output, shows four view items

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -5,15 +5,39 @@ import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons'
 
 const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors
 
-export default function WeatherDetails({ currentWeather }) {
+export default function WeatherDetails({ currentWeather, unitSystem }) {
 
     const {
         main: { feels_like, humidity, pressure },
         wind: { speed }
     } = currentWeather
 
+    const windSpeed = unitSystem === 'metric' ? `${ Math.round( speed ) } m/s` : `${ Math.round( speed ) } mph`
+
     return (
         <View style={ styles.weatherDetails }>
+            <View style={ styles.weatherDetailsRow }>
+                <View style={ {...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR} }>
+                    <View style={ styles.weatherDetailsRow }>
+                        <FontAwesome5 name="temperature-low" size={ 25 } color={ PRIMARY_COLOR } />
+                        <View style={ styles.weatherDetailsItems } >
+                            <Text>Feels like:</Text>
+                            <Text style={ styles.textSecondary }>{ feels_like }</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={ styles.weatherDetailsBox }>
+                    <View style={ {...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR} }>
+                        <View style={ styles.weatherDetailsRow }>
+                            <MaterialCommunityIcons name="water" size={ 30 } color={ PRIMARY_COLOR } />
+                            <View style={ styles.weatherDetailsItems } >
+                                <Text>Humidity:</Text>
+                                <Text style={ styles.textSecondary }>{ humidity }</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </View>
             <View style={ styles.weatherDetailsRow }>
                 <View style={ {...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR} }>
                     <View style={ styles.weatherDetailsRow }>
